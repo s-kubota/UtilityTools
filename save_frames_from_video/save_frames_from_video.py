c653c6d33891a6b027ea539fb1e19e9712f9b196
s-kubota <kubota_seiya@hotmail.co.jp>
2022-08-21 20:16:03 +0900

    MOD set time instead of frame for start and stop

--- save_frames_from_video/save_frames_from_video.py
+++ save_frames_from_video/save_frames_from_video.py
@@ -1,14 +1,14 @@
 import cv2
 import os
 
-def save_frames_from_video(video_path, start_frame, stop_frame, step_frame,
+def save_frames_from_video(video_path, start_time, stop_time, step_frame,
                      dir_path, basename, ext='jpg'):
     cap = cv2.VideoCapture(video_path)
 
     if not cap.isOpened():
         print("Video couldn't be opened.")
         return
-    
+
     fps = cap.get(cv2.CAP_PROP_FPS)
     if fps == 0:
         print("Error: fps is 0.")
@@ -17,10 +17,26 @@ def save_frames_from_video(video_path, start_frame, stop_frame, step_frame,
     frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     video_len_sec = frame_count / fps
     
-    print("Video info")
-    print(" FPS:", fps)
-    print(" Video length [s]:", video_len_sec)
+    print("Video info:")
+    print("  FPS:", fps)
+    print("  Video length [s]:", video_len_sec)
 
+    start_frame = int(start_time * fps)
+    stop_frame = int(stop_time * fps)
+
+    if start_frame < 0 or start_frame >= frame_count:
+        print("Error: Start time is out of video length.")
+        return
+
+    if stop_frame > frame_count:
+        stop_frame = frame_count
+    
+    if stop_frame == start_time:
+        stop_frame += 1
+    elif stop_frame < start_frame:
+        print("Error: Stop time should be more than or equal to start time.")
+        return
+    
     os.makedirs(dir_path, exist_ok=True)
     base_path = os.path.join(dir_path, basename)
 
@@ -36,10 +52,10 @@ def save_frames_from_video(video_path, start_frame, stop_frame, step_frame,
             return
 
 video_path = 'sample.MP4'
-start_frame = 0
-stop_frame = 100
+start_time = 0.0 # [s]
+stop_time = 0.0 # [s]
 step_frame = 10
 dir_path = 'data/temp/result'
 basename = 'video_frame'
 
-save_frames_from_video(video_path, start_frame, stop_frame, step_frame, dir_path, basename)
+save_frames_from_video(video_path, start_time, stop_time, step_frame, dir_path, basename)
